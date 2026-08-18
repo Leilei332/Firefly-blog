6ac5026219eaf462903062a7d903780ee8ddac43
Leilei332 <LeiYiXia29@outlook.com>
2026-08-18 10:46:18 +0800

    Prepare for netlify

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -46,7 +46,7 @@ export const siteConfig: SiteConfig = {
 	subtitle: "遗忘之地",
 
 	// 站点 URL
-	site_url: "https://firefly.cuteleaf.cn",
+	site_url: "https://xlbil-test.netlify.app",
 
 	// 站点描述
 	description:
@@ -127,7 +127,7 @@ export const siteConfig: SiteConfig = {
 	},
 
 	// 站点开始日期，用于统计运行天数
-	siteStartDate: "2025-01-01",
+	siteStartDate: "2026-08-18",
 
 	// 站点时区（IANA 时区字符串），用于格式化bangumi、rss里的构建日期时间等等..
 	// 示例："Asia/Shanghai", "UTC", 如果为空，则按照构建服务器的时区进行时区转换
@@ -233,7 +233,7 @@ export const siteConfig: SiteConfig = {
 		// 是否开启分享海报生成功能
 		sharePoster: true,
 		// OpenGraph图片功能，注意开启后要渲染很长时间，不建议本地调试的时候开启
-		generateOgImages: false,
+		generateOgImages: true,
 	},
 
 	// ── Bilibili配置 ──────────────────────────────────
